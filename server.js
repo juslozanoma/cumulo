527ccf263a46173a3221455a87b17cacf39513d4
Sebas <juslozanoma@gmail.com>
2026-07-14 21:51:35 -0500

    Set up Express server with webhook endpoint

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hola, mi bot está vivo!');
+});
+
+app.post('/webhook', (req, res) => {
+  console.log('Me llegó un mensaje:', req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
